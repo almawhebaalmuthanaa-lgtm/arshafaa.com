e01db90fa7b7ec65971cc10d775cbcae377c55a2
almawhebaalmuthanaa-lgtm <almawhebaalmuthanaa@gmail.com>
2026-04-19 22:18:27 +0300

    Create main Python file for weather dashboard application

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['POST'])
+def weather():
+    city = request.form['city']
+    api_key = 'YOUR_API_KEY'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    weather_data = response.json()
+    return render_template('weather.html', weather=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
